1c1fc01a9d66a65c0395d1bfdba211e049b94308
Olga_Smolyakova <Olga_Smolyakova@EPBYMINW5900.minsk.epam.com>
2019-03-07 13:49:56 +0300

    after change

--- src/main/java/com/epam/learn/TemperatureConverter.java
+++ src/main/java/com/epam/learn/TemperatureConverter.java
@@ -4,8 +4,11 @@ public class TemperatureConverter {
     /**
      * celsius to fahrenheit
      */
+	
     public int toFahrenheit(int celsius) {
 //        return celsius >= 0 ? (celsius * 9/5) + 32 : (celsius * 9/5) + 31;
         return (int)((celsius * 9.0/5.0) + 32);
     }
+    
+   
 }
